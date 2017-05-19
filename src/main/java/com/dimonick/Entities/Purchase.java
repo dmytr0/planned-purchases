56b9960ecf0c0e59bbc2078881f4bbc116e8c393
limon4ick <dimonick@bk.ru>
2017-05-19 20:09:01 +0300

    gen id

--- src/main/java/com/dimonick/Entities/Purchase.java
+++ src/main/java/com/dimonick/Entities/Purchase.java
@@ -7,6 +7,7 @@ import javax.persistence.*;
 public class Purchase {
 
     @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
     private Float amount;
     private String currency;
